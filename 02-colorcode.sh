4a6aa3a08b8132cd0c9d87e7d5e5cce74bdea2b3
Akash <phalke.aka@gmail.com>
2022-12-01 15:46:48 +0530

    update chnge

---
+++ 02-colorcode.sh
@@ -0,0 +1,11 @@
+echo -e "\e[32m i am new color[0m"
+
+echo -e "\e[33m new color introduced \e[0m"
+
+echo -e "\e[34m new color introduce \e[0m"
+
+echo -e "\e[35m new color inrtoduce \e[0m"
+
+echo -e "\e[33m i am color \e[0m]"
+
+echo =e "\e[34m i am color \e[0m"
