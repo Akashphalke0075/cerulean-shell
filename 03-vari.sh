1d1818a943aab960b58b6c308aaca9106eedd56e
Akash <phalke.aka@gmail.com>
2022-12-01 18:21:44 +0530

    changes

--- 03-vari.sh
+++ 03-vari.sh
@@ -2,8 +2,8 @@
 a=10
 b=20
 c=700
-date="22-dec-2022"
+today_date="22-dec-2022"
 echo "value of a is $a" and value of b is $b 
 echo "value of c is $c
 
-echo "good mornong, today date is $(date)
+echo "good mornong, today date is $(today_date)
