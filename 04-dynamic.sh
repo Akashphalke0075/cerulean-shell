44157148a33b9202d6f780cb0ba1bdde2bae533d
Akash <phalke.aka@gmail.com>
2022-12-01 19:48:31 +0530

    changes

--- 04-dynamic.sh
+++ 04-dynamic.sh
@@ -3,4 +3,5 @@
 today_date="$(date +%F)"
 no_users="$(who | wc-l)"
 
-echo "today date is ${today_date} and number of users are ${no_users}"
+echo "today date is ${today_date} 
+echo "number of users are : ${no_users}""
