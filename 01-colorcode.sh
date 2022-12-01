d5e96eca61b7ec7ec0861d92da22ae5370b7c59d
Akash <phalke.aka@gmail.com>
2022-12-01 15:26:13 +0530

    ypdate color

--- 01-colorcode.sh
+++ 01-colorcode.sh
@@ -5,3 +5,9 @@ echo -e "\e[32m i am color \e[0m"
 echo "adding another good color"
 
 echo -e "\e[33m another color \e[0m"
+
+echo -e "\e[34m new color \e[0m"
+
+echo -e "\e[35m new collor \e[0m"
+
+echo -e "\e[36m new color \e[0m
