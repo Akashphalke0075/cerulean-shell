bffd7f28d3e1b0263f028f38a43209772a877d93
Akash <phalke.aka@gmail.com>
2022-12-02 16:57:58 +0530

    update chnge

--- 07-specialvari.sh
+++ 07-specialvari.sh
@@ -3,5 +3,9 @@
 TODAY_DATE="$(date +%F)"
 TOTAL_COUNT="$(who | wc -l)"
 
+TOTAL_NO="$(who | wc -l)"
+
 echo "today date is ${TODAY_DATE}"
 echo "total session are : ${TOTAL_COUNT}"
+#printing line
+echo "total no of session are defined ${TOTAL_NO}"
