b69d7329b39398d48f0afe8c840228a6773e234f
Akash <phalke.aka@gmail.com>
2022-12-02 23:51:58 +0530

    update chnge

--- 08-specialvari.sh
+++ 08-specialvari.sh
@@ -6,3 +6,5 @@ TOTAL_COUNT="$(who | wc -l)"
 echo "good morning, today date is : ${TODAY_DATE}"
 
 echo "total no. of count is : ${TOTAL_COUNT}"
+
+echo -e "\e[33m good morning all. this color \e[0m"
