4e40a0d241c8dd91ddd52c00b6c241519364b01e
Akash <phalke.aka@gmail.com>
2022-12-02 23:56:40 +0530

    update chnge

--- 08-specialvari.sh
+++ 08-specialvari.sh
@@ -9,4 +9,5 @@ echo "good morning, today date is : ${TODAY_DATE}"
 echo "total no. of count is : ${TOTAL_COUNT}"
 
 echo -e "\e[33mgood morning all. this color and value is $a \e[0m"
-echo $#
+echo $1
+echo $2
